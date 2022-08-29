053451329758066f51be46b0710517759a38a3cd
dainel40911 <dainel4356899@gmail.com>
2022-08-30 02:17:01 +0800

    Updates

---
+++ precache-manifest.b82f18fc3bf2d95c011075cae740b404.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f5d15f3b94d8fa18441424e9da0c7c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9e27b5f6d7b884a1ebc3",
+    "url": "/home/static/css/main.d83a1963.chunk.css"
+  },
+  {
+    "revision": "0e7f5caaafdc0d1f8e85",
+    "url": "/home/static/js/2.7560e6ec.chunk.js"
+  },
+  {
+    "revision": "dff38bf37fc9eda4acb236ebba3264fd",
+    "url": "/home/static/js/2.7560e6ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e27b5f6d7b884a1ebc3",
+    "url": "/home/static/js/main.4c2c19ab.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/home/static/js/main.4c2c19ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ac38a88332ee199197",
+    "url": "/home/static/js/runtime-main.5ee548bd.js"
+  },
+  {
+    "revision": "5d9a93ed8ed26ff93df0b4522af42556",
+    "url": "/home/static/media/dainelsour.5d9a93ed.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
